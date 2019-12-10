aa40ae4cf9224d47ea2892cb5efaa7ba9f6aea6e
Pranav Singh <pranav_withyou@hotmail.com>
2019-12-10 17:14:43 +0530

    Added TPL project

---
+++ TPL/Console_App_Task_With_Cancellation.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TPL
+{
+    public static class Console_App_Task_With_Cancellation
+    {
+        public static int Fibonacci(int n, CancellationToken token)
+        {
+            int a = 0;
+            int b = 1;
+
+            for (int i = 0; i < n; i++)
+            {
+                if (token.IsCancellationRequested)
+                {
+                    Console.WriteLine("Getting out of the Fibonacci Sequence ....");
+
+                    return a;
+                }
+
+                int temp = a;
+                a = b;
+                b = temp + b;
+
+                Console.WriteLine($"Current result {a}");
+            }
+
+            return a;
+        }
+    }
+}
---
+++ TPL/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using static TPL.Console_App_Task_With_Cancellation;
+
+namespace TPL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleKeyInfo input;
+
+            CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+
+            CancellationToken token = cancellationTokenSource.Token;
+
+            Task<int> myFibonnaciTask = null;
+
+            myFibonnaciTask = new Task<int>(() => { return Fibonacci(5000, token); }, token);
+
+            myFibonnaciTask.Start();
+
+            do
+            {
+                while (!Console.KeyAvailable)
+                {
+                    if (myFibonnaciTask.IsCompleted)
+                    {
+                        goto End;
+                    }
+                }
+
+                input = Console.ReadKey(true);
+
+                if (!myFibonnaciTask.IsCompleted & !cancellationTokenSource.IsCancellationRequested)
+                {
+                    cancellationTokenSource.Cancel();
+
+                    Console.WriteLine("Cancelled");
+                }
+
+            } while (input.Key != ConsoleKey.Escape);
+
+        End:
+            Console.WriteLine($"Result {myFibonnaciTask.Result}");
+            Console.WriteLine("Program has ended!");
+            Console.ReadKey();
+
+        }
+    }
+}
